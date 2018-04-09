c39dd86c6b5009cb86ad435a580aac0b1d6471e6
cliu <cliu@presoftware.com.cn>
2018-04-09 12:36:53 +0800

    管理资源

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@
  * Created by presoft-00 on 2018/4/9.
  */
 import _ from 'lodash';
+import './style.css'
+import Icon from './icon.png'
 function component() {
     var element = document.createElement('div');
 
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+    element.classList.add('hello');
+    // 将图像添加到我们现有的 div。
+    var myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
     return element;
 }
 
